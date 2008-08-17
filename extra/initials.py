1a5982c7da5edd666576768deddfc99cfc7a8ff9
tunix <>
2008-08-17 23:10:12 +0000

    * extra/initials.py: converted the code to a class..

--- extra/initials.py
+++ extra/initials.py
@@ -11,31 +11,35 @@ Created by Alper KANAT on 2008-08-18.
 Copyright (c) 2008 raptiye. All rights reserved.
 """
 
-def create_anonymous_user():
-	from django.conf import settings
-	from django.contrib.auth.models import User
-	user = User.objects.create_user("anonymous", settings.EMAIL_INFO_ADDRESS_EN, settings.ANONYMOUS_PASSWORD)
-	user.first_name = "Anonymous"
-	user.last_name = "User"
-	user.save()
-	user.profile.create()
+class Initials():
 
-def create_profile_for_first_user():
-	"""
-	When a Django application is first created,
-	it asks permission to create a user who has
-	the administrator privileges. Usually this 
-	user doesn't have a profile..
-	
-	This method creates the profile for that user.
-	"""
-	from django.contrib.auth.models import User
-	user = User.objects.get(pk=1)
-	user.profile.create()
+	def __init__(self):
+		self.create_anonymous_user()
+		self.create_profile_for_first_user()
 
-def main():
-	create_anonymous_user()
-	create_profile_for_first_user()
+	def create_anonymous_user(self):
+		"""
+		Creates the anonymous user that will be
+		the new raptiye user of all comments that
+		were entered earlier..
+		"""
+		from django.conf import settings
+		from django.contrib.auth.models import User
+		user = User.objects.create_user("anonymous", settings.EMAIL_INFO_ADDRESS_EN, settings.ANONYMOUS_PASSWORD)
+		user.first_name = "Anonymous"
+		user.last_name = "User"
+		user.save()
+		user.profile.create()
 
-if __name__ == '__main__':
-	main()
+	def create_profile_for_first_user(self):
+		"""
+		When a Django application is first created,
+		it asks permission to create a user who has
+		the administrator privileges. Usually this 
+		user doesn't have a profile..
+		
+		This method creates the profile for that user.
+		"""
+		from django.contrib.auth.models import User
+		user = User.objects.get(pk=1)
+		user.profile.create()
