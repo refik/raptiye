dd93827ced4ce388b96d57fcc5f23aca4edb4afb
Alper KANAT <alperkanat@raptiye.org>
2009-01-04 00:25:51 +0200

    * TODO, settings-example.py: rearranged all file in alphabetical and group order..

--- settings-example.py
+++ settings-example.py
@@ -18,46 +18,108 @@ import os
 
 DOCUMENT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
+
+# --- ADMIN SETTINGS ------------------------
+
+ADMIN_LIST_PER_PAGE = 20
+AUTH_PROFILE_MODULE = 'users.UserProfile'
+
+EMAIL_FAIL_SILENCE = False
+EMAIL_HOST = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_HOST_USER = "bilgi@raptiye.org"
+# this address is used for notifications
+EMAIL_INFO_ADDRESS_TR = "bilgi@raptiye.org"
+# EMAIL_PORT = 
+EMAIL_SUBJECT_PREFIX = u"[raptiye] "
+EMAIL_USE_TLS = False
+
+INSTALLED_APPS = (
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.flatpages',
+	'django.contrib.redirects',
+	'django.contrib.sessions',
+	'django.contrib.sites',
+	'raptiye.blog',
+	'raptiye.comments',
+	'raptiye.contrib.session_messages',
+	'raptiye.frontpage',
+	'raptiye.links',
+	'raptiye.polls',
+	'raptiye.tags',
+	'raptiye.users',
+)
+
+LANGUAGES = (
+	("tr", "tr"),
+	("en", "en"),
+)
+
+LOGIN_URL = "/users/login/"
+LOGOUT_URL = "/users/logout/"
+
+# URL Pattern Naming used here..
+REDIRECT_URL = "blog"
+
+# fill this with a newly created django app's secret key
+SECRET_KEY = ''
+
+# this is to serve temporary images like captcha..
+TEMP_MEDIA_PREFIX = 'temp/'
+
+
+# --- BLOG SETTINGS -------------------------
+
+# colorize the code in entries
+COLORIZE_CODE = True
+ENTRIES_PER_PAGE = 5
+
+
+# --- CAPTCHA SETTINGS ----------------------
+
+# how many minutes of penaly will be applied..
+CAPTCHA_PENALTY = 10
+# how many times a user can regenerate captchas consequtively
+CAPTCHA_RENEWAL_LIMIT = 10
+
+
+# --- COMMENT SETTINGS ----------------------
+
+# enable or disable writing comments anonymously
+ALLOW_ANONYMOUS_COMMENTS = True
+ANONYMOUS_PASSWORD = ""
+LATEST_COMMENTS_LIMIT = 5
+
+
+# --- DATABASE SETTINGS ---------------------
+
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_HOST = ''
+DATABASE_NAME = '%s/raptiye.db' % DOCUMENT_ROOT
+DATABASE_PASSWORD = ''
+DATABASE_PORT = ''
+DATABASE_USER = ''
+
+
+# --- DEBUG SETTINGS ------------------------
 
 ADMINS = (
     ('Alper KANAT', 'alperkanat@raptiye.org'),
 )
 
+AUTHENTICATION_BACKENDS = (
+	'django.contrib.auth.backends.ModelBackend',
+	'raptiye.users.backends.OpenIDBackend',
+)
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = '%s/raptiye.db' % DOCUMENT_ROOT
-DATABASE_USER = ''
-DATABASE_PASSWORD = ''
-DATABASE_HOST = ''
-DATABASE_PORT = ''
 
-TIME_ZONE = 'Europe/Istanbul'
-
-LANGUAGE_CODE = 'tr'
-DEFAULT_CHARSET='utf8'
-DEFAULT_CONTENT_TYPE = 'text/html'
-FILE_CHARSET = 'utf-8'
-
-SITE_ID = 1
-
-USE_I18N = True
-
-MEDIA_ROOT = '%s/media/' % DOCUMENT_ROOT
-
-MEDIA_URL = '/media/'
-
-ADMIN_MEDIA_PREFIX = '/media/admin/'
-
-# fill this with a newly created django app's secret key
-SECRET_KEY = ''
-
-TEMPLATE_LOADERS = (
-	'django.template.loaders.filesystem.load_template_source',
-	'django.template.loaders.app_directories.load_template_source',
-)
+# --- DJANGO INTERNAL SETTINGS --------------
 
 MIDDLEWARE_CLASSES = (
 	'django.middleware.common.CommonMiddleware',
@@ -66,32 +128,6 @@ MIDDLEWARE_CLASSES = (
 	'django.middleware.doc.XViewMiddleware',
 	'django.contrib.redirects.middleware.RedirectFallbackMiddleware',
 	'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware'
-)
-
-ROOT_URLCONF = 'raptiye.urls'
-
-TEMPLATE_DIRS = (
-	"%s/templates/" % DOCUMENT_ROOT,
-)
-
-INSTALLED_APPS = (
-	'django.contrib.admin',
-	'django.contrib.auth',
-	'django.contrib.contenttypes',
-	'django.contrib.redirects',
-	'django.contrib.sessions',
-	'django.contrib.sites',
-	'django.contrib.flatpages',
-	'raptiye.blog',
-	'raptiye.comments',
-	'raptiye.frontpage',
-	'raptiye.tags',
-	'raptiye.users',
-	'raptiye.links',
-	'raptiye.polls',
-	# the following app(s) aren't mandatory and will be removed
-	# when django has the functionality
-	'raptiye.contrib.session_messages',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
@@ -103,70 +139,65 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 	"raptiye.contrib.session_messages.context_processors.session_messages",
 )
 
-AUTHENTICATION_BACKENDS = (
-	'django.contrib.auth.backends.ModelBackend',
-	'raptiye.users.backends.OpenIDBackend',
+TEMPLATE_DIRS = (
+	"%s/templates/" % DOCUMENT_ROOT,
 )
 
-ADMIN_LIST_PER_PAGE = 20
-ENTRIES_PER_PAGE = 5
-LANGUAGES = (
-	("tr", "tr"),
-	("en", "en"),
+TEMPLATE_LOADERS = (
+	'django.template.loaders.filesystem.load_template_source',
+	'django.template.loaders.app_directories.load_template_source',
 )
 
-AUTH_PROFILE_MODULE = 'users.UserProfile'
-# this is to serve temporary images like captcha..
-TEMP_MEDIA_PREFIX = 'temp/'
-# how many times a user can regenerate captchas consequtively
-CAPTCHA_RENEWAL_LIMIT = 10
-# how many minutes of penaly will be applied..
-CAPTCHA_PENALTY = 10
 
-# this address is used for notifications
-EMAIL_INFO_ADDRESS_TR = "bilgi@raptiye.org"
+# --- LANGUAGE SETTINGS ---------------------
 
-# MAIL SETTINGS (needs to be changed..)
-EMAIL_HOST = ""
-# EMAIL_PORT = 
-EMAIL_HOST_USER = EMAIL_INFO_ADDRESS_TR
-EMAIL_HOST_PASSWORD = ""
-EMAIL_SUBJECT_PREFIX = u"[raptiye] "
-EMAIL_USE_TLS = False
-EMAIL_FAIL_SILENCE = False
+DEFAULT_CHARSET='utf8'
+DEFAULT_CONTENT_TYPE = 'text/html'
+FILE_CHARSET = 'utf-8'
+LANGUAGE_CODE = 'tr'
 
-# default avatar for all users..
-DEFAULT_AVATAR = "/media/images/default_avatar.png"
-
-# LOCALE settings may differ on different platforms
 LOCALES = {
 	'tr': 'tr_TR.utf-8',
 	'en': 'en_US.utf-8',
 }
 
+USE_I18N = True
+
+
+# --- PROFILE SETTINGS ----------------------
+
+# default avatar for all users..
+DEFAULT_AVATAR = "/media/images/default_avatar.png"
+
+
+# --- RAPTIYE SETTINGS ----------------------
+
 VERSION = '1.0.3'
 
-ANONYMOUS_PASSWORD = ""
-LATEST_COMMENTS_LIMIT = 5
 
-# enable or disable writing comments anonymously
-ALLOW_ANONYMOUS_COMMENTS = True
+# --- TIME SETTINGS -------------------------
 
-# twitter credentials
+TIME_ZONE = 'Europe/Istanbul'
+
+
+# --- TWITTER SETTINGS ----------------------
+
+POST_TO_TWITTER = True
 TWITTER_USERNAME = ""
 TWITTER_PASSWORD = ""
 TWITTER_LIMIT = 5
-POST_TO_TWITTER = True
 
-# colorize the code in entries
-COLORIZE_CODE = True
 
-# OpenID Support
+# --- OPENID SETTINGS -----------------------
+
 OPENID = True
 OPENID_PASSWORD_FOR_NEW_USER = ""
 
-LOGIN_URL = "/users/login/"
-LOGOUT_URL = "/users/logout/"
 
-# URL Pattern Naming used here..
-REDIRECT_URL = "blog"
+# --- URL SETTINGS --------------------------
+
+ADMIN_MEDIA_PREFIX = '/media/admin/'
+MEDIA_ROOT = '%s/media/' % DOCUMENT_ROOT
+MEDIA_URL = '/media/'
+ROOT_URLCONF = 'raptiye.urls'
+SITE_ID = 1
